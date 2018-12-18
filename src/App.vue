d5a9b74da2acecf6eb74d3abe52282d6f04bd35b
Alex Gretschann <me@copypast.de>
2018-12-18 07:48:58 +0100

    added profile and repository request

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template lang="html">
 <v-form>
   <v-container fluid>
-    <v-layout row wrap>
+    <v-layout column wrap>
 
       <v-flex xs-12>
         <h2 class="display-3 font-weight-black">Search for users</h2>
@@ -16,7 +16,7 @@
         </v-text-field>
       </v-flex> -->
 
-      <v-flex xs-12 class="pa-3">
+      <v-flex xs-6 class="pa-3">
         <v-autocomplete
           v-model="user"
           :items="users"
@@ -33,21 +33,34 @@
         </v-autocomplete>
       </v-flex>
 
-      <v-flex xs-12>
+      <v-flex xs12 v-if="user">
+       <v-layout row wrap>
 
-        <v-layout row wrap v-if="user">
-          <v-flex xs-4>
-            <img :src="user.avatar_url" alt="">
-            <h4>{{user.login}}</h4>
-          </v-flex>
-          <v-flex xs-8>
-          </v-flex>
-        </v-layout>
+         <v-flex xs5 style="padding:8px">
+           <div class="">
+             <img :src="user.avatar_url" alt="" style="max-width:100%">
+             <h4 class="display-1">{{user.login}}</h4>
+             <p>Since: {{userProfile.created_at}}</p>
+           </div>
+         </v-flex>
 
-        <pre>
-          {{user}}
-        </pre>
-      </v-flex>
+         <v-flex xs7 style="padding:8px">
+           <v-layout row wrap>
+             <v-flex>
+              <h3 class="font-weight-black pb-4">Saved repositorys</h3>
+              <div v-for="repo in userRepos">
+                <p class="font-weight-black">
+                  <a :href="repo.html_url">
+                    {{repo.full_name}}
+                  </a>
+                </p>
+              </div>
+             </v-flex>
+           </v-layout>
+         </v-flex>
+
+       </v-layout>
+     </v-flex>
 
     </v-layout>
   </v-container>
@@ -65,23 +78,22 @@ export default {
       users: [],
       user: null,
       isLoading: false,
-      search: null
+      search: null,
+      userRepos: null,
+      userProfile: null
     }
   },
   watch: {
     search (val) {
-
-      console.log('val', val, val.length);
-
       if(val.length > 4) {
         this.loadData(val);
       }
-
       // Items have already been requested
       if (this.isLoading) return
-
       this.isLoading = true
-
+    },
+    user () {
+      this.getUserRepos()
     }
   },
   methods: {
@@ -89,7 +101,6 @@ export default {
       // Lazily load input items
       axios.get(`https://api.github.com/search/users?q=${this.search}`)
         .then(res => {
-          console.log('data',res);
           const { count, entries } = res.data
           this.users = res.data.items
           // this.count = count
@@ -99,6 +110,17 @@ export default {
           console.log(err)
         })
         .finally(() => (this.isLoading = false))
+    },
+    getUserRepos() {
+      axios.get(`https:/api.github.com/users/gretschann/repos`)
+        .then(repos => {
+          this.userRepos = repos.data
+        })
+
+      axios.get(`https:/api.github.com/users/gretschann`)
+        .then(profile => {
+          this.userProfile = profile.data
+        })
     }
   }
 }
